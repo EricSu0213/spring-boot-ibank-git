8174465fc644a87a7381a11c0dc8cd97097e6a57
EricSU0213 <tokio5v6@gmail.com>
2019-04-22 15:14:01 +0800

    ibank package into war

--- src/main/java/com/example/ibank/IbankApplication.java
+++ src/main/java/com/example/ibank/IbankApplication.java
@@ -5,17 +5,25 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.example.ibank.repository.AccountRepository;
 
 @SpringBootApplication
-public class IbankApplication {
+public class IbankApplication extends SpringBootServletInitializer{
 
     @Autowired
     DataSource dataSource;
 
     @Autowired
     AccountRepository accountRepository;
+
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(IbankApplication.class);
+    }
 
     public static void main(String[] args) {
 		SpringApplication.run(IbankApplication.class, args);
