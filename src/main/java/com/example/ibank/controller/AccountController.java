aaf9d44711db15908ca3b37ee19af5578181d182
EricSU0213 <tokio5v6@gmail.com>
2019-04-23 16:39:13 +0800

    add admin/deleteUser

--- src/main/java/com/example/ibank/controller/AccountController.java
+++ src/main/java/com/example/ibank/controller/AccountController.java
@@ -1,15 +1,19 @@
 package com.example.ibank.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.ibank.entity.Account;
 import com.example.ibank.service.AccountService;
 
 @Controller
@@ -20,7 +24,7 @@ public class AccountController {
 	
 	@ResponseBody
 	@RequestMapping(value="/deleteAccount", method = RequestMethod.GET)
-    public Boolean registration() throws Exception {
+    public Boolean deleteAccount() throws Exception {
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
@@ -39,4 +43,13 @@ public class AccountController {
         return Boolean.TRUE;
     }
 
+	@ResponseBody
+	@RequestMapping(value="/admin/deleteUser", method = RequestMethod.POST)
+    public Boolean deleteUser(@RequestBody Account account, HttpServletRequest request) throws Exception {	
+		
+		String email = account.getEmail();
+		accountService.deleteAccount(email);
+		
+        return Boolean.TRUE;
+    }
 }
