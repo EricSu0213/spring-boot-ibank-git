56b64f46fd9c4d7d6f18477f46e5361a64c016f8
Eric Su <tokio5v6@gmail.com>
2019-06-20 14:28:16 +0800

    contextpath bug

---
+++ src/main/resources/static/js/getContextPath.js
@@ -0,0 +1,5 @@
+function getContextPath() {
+    var ctx = window.location.pathname,
+        path = '/' !== ctx ? ctx.substring(0, ctx.indexOf('/', 1) + 1) : ctx;
+    return path + (/\/$/.test(path) ? '' : '/');
+}
